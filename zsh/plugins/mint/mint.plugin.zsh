f63b6eef98a265edd98a8735ef8ef36c10be96dd
SpritzerPyro <pyro@outlook.at>
2022-09-07 12:55:16 +0200

    Fix shellcheck errors in the mint zsh plugin

--- zsh/plugins/mint/mint.plugin.zsh
+++ zsh/plugins/mint/mint.plugin.zsh
@@ -1,4 +1,6 @@
-(( $+commands[apt] )) && _apt_cmd=apt || _apt_cmd=apt-get
+[[ "${commands[apt]:-}" ]] && _apt_cmd=apt || _apt_cmd=apt-get
+
+echo "${_apt_cmd}"
 
 function snu() {
   local snapname revision
@@ -11,10 +13,10 @@ function snu() {
     done
 }
 
-alias snid="sudo rm --force /etc/apt/preferences.d/nosnap.pref; sudo ${_apt_cmd} update; sudo ${_apt_cmd} install snapd --yes"
+alias snid='sudo rm --force /etc/apt/preferences.d/nosnap.pref; sudo ${_apt_cmd} update; sudo ${_apt_cmd} install snapd --yes'
 
-alias apu="sudo ${_apt_cmd} update; sudo ${_apt_cmd} upgrade --with-new-pkgs; sudo ${_apt_cmd} dist-upgrade; sudo ${_apt_cmd} autoremove; sudo ${_apt_cmd} autoclean"
-alias apu!="sudo ${_apt_cmd} update; sudo ${_apt_cmd} upgrade --with-new-pkgs --yes; sudo ${_apt_cmd} dist-upgrade --yes; sudo ${_apt_cmd} autoremove --yes; sudo ${_apt_cmd} autoclean --yes"
+alias apu='sudo ${_apt_cmd} update; sudo ${_apt_cmd} upgrade --with-new-pkgs; sudo ${_apt_cmd} dist-upgrade; sudo ${_apt_cmd} autoremove; sudo ${_apt_cmd} autoclean'
+alias apu!='sudo ${_apt_cmd} update; sudo ${_apt_cmd} upgrade --with-new-pkgs --yes; sudo ${_apt_cmd} dist-upgrade --yes; sudo ${_apt_cmd} autoremove --yes; sudo ${_apt_cmd} autoclean --yes'
 
-alias upa="apu; snu; omd update"
-alias upa!="apu!; snu; omd update"
+alias upa='apu; snu; omd update'
+alias upa!='apu!; snu; omd update'
