31aaf1f1cc6b2712562abfde69cb78e2da08d411
SpritzerPyro <pyro@outlook.at>
2022-09-18 10:02:16 +0200

    Add var to disable commit template in scrum plugin

--- zsh/plugins/scrum/scrum.plugin.zsh
+++ zsh/plugins/scrum/scrum.plugin.zsh
@@ -23,6 +23,21 @@ function sgi() {
 function ssi() {
   echo "${1:-}" > "${SCRUM_PLUGIN_DIR}/.number"
 
+  local -r issue=$(sgi)
+
+  if [[ "${issue}" ]]; then
+    echo "Scrum Plugin: Set issue to '${issue}'"
+  else
+    echo "Scrum Plugin: Clear issue"
+  fi
+
+  if \
+    [[ "${SCRUM_PLUGIN_COMMIT_TEMPLATE:-1}" == "0" ]] || \
+    [[ "${SCRUM_PLUGIN_COMMIT_TEMPLATE:-1}" == "false" ]] \
+  ; then
+    return
+  fi
+
   local file
 
   file="$(git config commit.template || echo "")"
@@ -38,17 +53,13 @@ function ssi() {
 
   mkdir -p "${file:A:h}"
 
-  local -r original=$(cat "${file}" 2>/dev/null || echo "")
-  local -r issue=$(sgi)
-
   if [[ "${issue}" ]]; then
-    echo "Scrum Plugin: Set issue to '${issue}'"
     echo "Scrum Plugin: Add '[${issue}]' to '${file}'"
   else
-    echo "Scrum Plugin: Clear issue"
     echo "Scrum Plugin: Remove issue from '${file}'"
   fi
 
+  local -r original=$(cat "${file}" 2>/dev/null || echo "")
   local -r content="$(
     echo
     {
@@ -68,16 +79,21 @@ function ssi() {
   echo "${content}" > "${file}"
 }
 
-_scrum_plugin_file="$(git config commit.template || echo "")"
-_scrum_plugin_file="${_scrum_plugin_file/#\~/"${HOME}"}"
+if \
+  [[ "${SCRUM_PLUGIN_COMMIT_TEMPLATE:-1}" != "0" ]] && \
+  [[ "${SCRUM_PLUGIN_COMMIT_TEMPLATE:-1}" != "false" ]] \
+; then
+  _scrum_plugin_file="$(git config commit.template || echo "")"
+  _scrum_plugin_file="${_scrum_plugin_file/#\~/"${HOME}"}"
 
-if [[ "${_scrum_plugin_file}" ]] && [[ ! -f "${_scrum_plugin_file}" ]]; then
-  echo "Scrum Plugin: Create not existing '${_scrum_plugin_file}'"
-  mkdir -p "${_scrum_plugin_file:A:h}"
-  touch "${_scrum_plugin_file}"
+  if [[ "${_scrum_plugin_file}" ]] && [[ ! -f "${_scrum_plugin_file}" ]]; then
+    echo "Scrum Plugin: Create not existing '${_scrum_plugin_file}'"
+    mkdir -p "${_scrum_plugin_file:A:h}"
+    touch "${_scrum_plugin_file}"
+  fi
+
+  unset _scrum_plugin_file
 fi
 
 : "${SCRUM_ISSUE_PREFIX:=JIRA}"
 : "${SCRUM_PLUGIN_DIR:="${0:A:h}"}"
-
-unset _scrum_plugin_file
