2340f0e24346aa408fa35e898f1b9d9fc2f490a6
SpritzerPyro <pyro@outlook.at>
2022-11-13 12:13:43 +0100

    Add danger level and zone variables

--- prodfiles.sh
+++ prodfiles.sh
@@ -9,6 +9,24 @@
 : "${HIST_STAMPS:="yyyy-mm-dd"}"
 : "${TERM:="xterm-256color"}"
 
+if ! typeset -f prodfiles_danger_zone >/dev/null; then
+  function prodfiles_danger_zone() {
+    [[ "${SSH_CONNECTION:-}" ]]
+  }
+fi
+
+: "${PRODFILES_DANGER_ZONE:="$(prodfiles_danger_zone && echo 1 || echo 0)"}"
+
+if (( PRODFILES_DANGER_ZONE )); then
+  : "${PRODFILES_DANGER_LEVEL:=2}"
+fi
+
+if [[ "${SSH_CONNECTION:-}" ]]; then
+  : "${PRODFILES_DANGER_LEVEL:=1}"
+fi
+
+: "${PRODFILES_DANGER_LEVEL:=0}"
+
 if [[ "${TERM}" == "tmux-256color" ]]; then
   TERM="xterm-256color"
 fi
