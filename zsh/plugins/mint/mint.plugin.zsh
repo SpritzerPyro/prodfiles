ab7b2c89e846a362d6426e444dbe24120f11752d
SpritzerPyro <pyro@outlook.at>
2022-11-13 13:05:22 +0100

    Add some flexibility to the mint plugin

--- zsh/plugins/mint/mint.plugin.zsh
+++ zsh/plugins/mint/mint.plugin.zsh
@@ -1,28 +1,73 @@
 # shellcheck disable=SC2139
 
-[[ "${commands[apt]:-}" ]] && _apt_cmd=apt || _apt_cmd=apt-get
+# Aptitude
 
-function ati() {
+function apt_test_installed() {
   dpkg-query -Wf'${db:Status-abbrev}' "$1" 2>/dev/null | grep -q '^i'
 }
 
-function snu() {
+function apt_update_all () {
+  local _apt_cmd _sudo_cmd
+
+  [[ "${commands[apt]:-}" ]] && _apt_cmd="apt" || _apt_cmd="apt-get"
+  [[ "${commands[sudo]:-}" ]] && _sudo_cmd="sudo " || _sudo_cmd=""
+
+  apt_test_installed "${_apt_cmd}" || return 0
+
+  eval "${_sudo_cmd}${_apt_cmd} update $*"
+  eval "${_sudo_cmd}${_apt_cmd} upgrade --with-new-pkgs $*"
+  eval "${_sudo_cmd}${_apt_cmd} dist-upgrade $*"
+  eval "${_sudo_cmd}${_apt_cmd} autoremove $*"
+  eval "${_sudo_cmd}${_apt_cmd} autoclean $*"
+}
+
+alias apu!="apt_update_all --yes"
+alias apu="apt_update_all"
+alias ati="apt_test_installed"
+
+# Snap
+
+function snap_install() {
+  local _apt_cmd _sudo_cmd
+
+  [[ "${commands[apt]:-}" ]] && _apt_cmd="apt" || _apt_cmd="apt-get"
+  [[ "${commands[sudo]:-}" ]] && _sudo_cmd="sudo " || _sudo_cmd=""
+
+  if ! apt_test_installed "${_apt_cmd}"; then
+    echo "Command '${_apt_cmd}' not found" >&2
+    return 1
+  fi
+
+  eval "${_sudo_cmd}rm --force /etc/apt/preferences.d/nosnap.pref"
+  eval "${_sudo_cmd}${_apt_cmd} update"
+  eval "${_sudo_cmd}${_apt_cmd} install snapd --yes"
+}
+
+function snap_update() {
+  local _sudo_cmd
+
+  [[ "${commands[sudo]:-}" ]] && _sudo_cmd="sudo " || _sudo_cmd=""
+
+  if ! apt_test_installed snapd; then
+    echo "The snap daemon is not installed."
+    echo "To install snap execute \"snap_install\""
+
+    return
+  fi
+
   local snapname revision
 
-  sudo snap refresh
+  eval "${_sudo_cmd}snap refresh"
 
   LANG=C snap list --all | awk '/disabled/{ print $1, $3 }' |
     while read -r snapname revision; do
-        sudo snap remove "${snapname}" --revision="${revision}"
+        eval "${_sudo_cmd}snap remove ${snapname} --revision=${revision}"
     done
 }
 
-alias snid="sudo rm --force /etc/apt/preferences.d/nosnap.pref; sudo ${_apt_cmd} update; sudo ${_apt_cmd} install snapd --yes"
+alias sup="snap_update"
 
-alias apu="sudo ${_apt_cmd} update; sudo ${_apt_cmd} upgrade --with-new-pkgs; sudo ${_apt_cmd} dist-upgrade; sudo ${_apt_cmd} autoremove; sudo ${_apt_cmd} autoclean"
-alias apu!="sudo ${_apt_cmd} update; sudo ${_apt_cmd} upgrade --with-new-pkgs --yes; sudo ${_apt_cmd} dist-upgrade --yes; sudo ${_apt_cmd} autoremove --yes; sudo ${_apt_cmd} autoclean --yes"
+# Joint operations
 
-alias upa="apu; snu; omd update"
-alias upa!="apu!; snu; omz update"
-
-unset _apt_cmd
+alias upa="apt_update_all; snap_update; omd update"
+alias upa!="apt_update_all --yes; snap_update; omz update"
