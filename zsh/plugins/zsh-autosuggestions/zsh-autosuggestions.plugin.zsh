9c19a7b6eb3186eaca43e3d127dbcfdeec556696
SpritzerPyro <pyro@outlook.at>
2023-03-19 10:13:39 +0100

    Remove the custom zsh-autosuggestions clone

--- zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+++
@@ -1,6 +0,0 @@
-if [[ ! -d "${0:A:h}/zsh-autosuggestions" ]]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git \
-    "${0:A:h}/zsh-autosuggestions"
-fi
-
-source "${0:A:h}/zsh-autosuggestions/zsh-autosuggestions.zsh"
